54c4d05c51990e312797ec12b93cdc1218dd2757
ewangclarkson <ewangclarks@gmail.com>
2024-04-08 17:40:30 +0100

    added gateway prod url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,10 +1,10 @@
-const apiHost='http://localhost:8000';
+const apiHost='https://pk-api-gateway.onrender.com';
 
 export const environment = {
   apiHost:apiHost,
   userApiUrl:`${apiHost}/user-service`,
   packageApiUrl:`${apiHost}/package-service`,
   deliveryApiUrl:`${apiHost}/delivery-service`,
-  wsHost:'http://localhost:8002',
+  wsHost:'https://pk-delivery-service.onrender.com',
   googleMapApiKey:"AIzaSyCbbqBUVGdfxZ7okId4BVM_Dyc2jePnyRk"
 };
